4658ddca50c76c403aa12f708fcb6b96ae4f4359
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-08-18 17:16:00 -0400

    Added SCSS variables. Tested JS script for making nav link active.

---
+++ js/app.js
@@ -0,0 +1,50 @@
+/* ======================================
+    Dashboard
+   ====================================== */
+// Variables
+const pageURL = window.location.href.toLowerCase();
+// const $navURL = $(this).attr('href');
+
+// Make nav link active
+// If nav link URL matches current page URL...
+// if (pageURL.includes($navURL)) {
+//   // add class "active" to nav link
+//   $('.nav__item').addClass('active');
+// }
+
+// // Loop through each nav item
+// $('.nav__item').each( function (index, navURL) {
+//   const $navURL = $(this).attr('href');
+//
+// // If nav link URL matches current page URL...
+//   if ($navURL === 'index.html') {
+//     // add class "active" to nav link
+//     $(this).addClass('active');
+//   }
+//   // else remove "active" class from nav link
+//   else {
+//     $(this).removeClass('active');
+//   }
+//
+// });
+
+if (pageURL.includes('index.html')) {
+  // $('.nav_item').addClass('active');
+  console.log("It's in there.");
+}
+
+// // Loop through each nav item
+// $('.nav__item').each( function (index, navURL) {
+//   const $navURL = $(this).attr('href').toLowerCase();
+//
+// // If nav link URL matches current page URL...
+//   if (pageURL.includes($navURL)) {
+//     // add class "active" to nav link
+//     $(this).addClass('active');
+//   }
+//   // else remove "active" class from nav link
+//   else {
+//     $(this).removeClass('active');
+//   }
+//
+// });
