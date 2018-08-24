b72c6eea9bac8c15900855987992e2c339acbc6e
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-08-24 06:07:10 -0400

    Moved scripts for adding class "active" to nav link from app.js to nav-active.js for future use in other projects.

--- js/app.js
+++ js/app.js
@@ -14,8 +14,9 @@ $('.nav-item').each( function (index, navURL) {
     // add class "active" to nav link
     $(this).addClass('active');
   }
+  // If current page URL is the home page without "index.html" specified
   else if (pageURL.endsWith('fewd-techdegree-project-7/')) {
-    // add class "active" to nav link
+    // add class "active" to the first nav link (for the dashboard)
     $('.nav-item:first-child').addClass('active');
   }
   // else remove "active" class from nav link
---
+++ js/nav-active.js
@@ -0,0 +1,27 @@
+/* ======================================
+    Nav Link Activator
+   ====================================== */
+
+   // Variables
+   const pageURL = window.location.href.toLowerCase();
+
+   // Make nav link active
+   // Loop through each nav item
+   $('.nav-item').each( function (index, navURL) {
+     const $navURL = $(this).attr('href').toLowerCase();
+
+   // If nav link URL matches current page URL...
+     if (pageURL.includes($navURL)) {
+       // add class "active" to nav link
+       $(this).addClass('active');
+     }
+     // If current page URL is the home page without "index.html" specified
+     else if (pageURL.endsWith('fewd-techdegree-project-7/')) {
+       // add class "active" to the first nav link (for the dashboard)
+       $('.nav-item:first-child').addClass('active');
+     }
+     // else remove "active" class from nav link
+     else {
+       $(this).removeClass('active');
+     }
+   });
