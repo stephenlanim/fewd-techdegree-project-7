44a0aa928959c489a51148c1ab1a1834c70fca0f
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-08-24 05:57:32 -0400

    Changed .nav__item class name to .nav-item. Debugged script for applying "active" class on nav-item.

--- js/app.js
+++ js/app.js
@@ -6,13 +6,17 @@ const pageURL = window.location.href.toLowerCase();
 
 // Make nav link active
 // Loop through each nav item
-$('.nav__item').each( function (index, navURL) {
+$('.nav-item').each( function (index, navURL) {
   const $navURL = $(this).attr('href').toLowerCase();
 
 // If nav link URL matches current page URL...
   if (pageURL.includes($navURL)) {
     // add class "active" to nav link
     $(this).addClass('active');
+  }
+  else if (pageURL.endsWith('fewd-techdegree-project-7/')) {
+    // add class "active" to nav link
+    $('.nav-item:first-child').addClass('active');
   }
   // else remove "active" class from nav link
   else {
