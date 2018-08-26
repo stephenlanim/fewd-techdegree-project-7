8ab7e20face5232063e960c9d4a902eade9be528
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-08-26 16:54:15 -0400

    Embedded preliminary charts. Added borders to widgets.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,175 @@
 /* ======================================
     Dashboard
    ====================================== */
+
+/* ===== Traffic Chart ===== */
+
+var $trafficCanvas = $('canvas#trafficChart');
+var trafficLineChart = new Chart($trafficCanvas, {
+   type: 'bar',
+   data: {
+       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+       datasets: [{
+           label: '# of Votes',
+           data: [12, 19, 3, 5, 2, 3],
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255,99,132,1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 1
+       }]
+   },
+   options: {
+       maintainAspectRatio: false,
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   },
+   responsive: true
+});
+
+$trafficCanvas.parent().css({'width': '80vw','height': '40vh'});
+
+
+/* ===== Daily Traffic Chart ===== */
+
+var $dailyTrafficCanvas = $('canvas#dailyTrafficChart');
+var dailyTrafficBarChart = new Chart($dailyTrafficCanvas, {
+   type: 'bar',
+   data: {
+       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+       datasets: [{
+           label: '# of Votes',
+           data: [12, 19, 3, 5, 2, 3],
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255,99,132,1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 1
+       }]
+   },
+   options: {
+       maintainAspectRatio: false,
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   },
+   responsive: true
+});
+
+$dailyTrafficCanvas.parent().css({'width': '80%','height': '70%'});
+
+
+/* ===== Mobile Users Chart ===== */
+
+var $mobileUsersCanvas = $('canvas#mobileUsersChart');
+var mobileUserDonutChart = new Chart($mobileUsersCanvas, {
+   type: 'bar',
+   data: {
+       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+       datasets: [{
+           label: '# of Votes',
+           data: [12, 19, 3, 5, 2, 3],
+           backgroundColor: [
+               'rgba(255, 99, 132, 0.2)',
+               'rgba(54, 162, 235, 0.2)',
+               'rgba(255, 206, 86, 0.2)',
+               'rgba(75, 192, 192, 0.2)',
+               'rgba(153, 102, 255, 0.2)',
+               'rgba(255, 159, 64, 0.2)'
+           ],
+           borderColor: [
+               'rgba(255,99,132,1)',
+               'rgba(54, 162, 235, 1)',
+               'rgba(255, 206, 86, 1)',
+               'rgba(75, 192, 192, 1)',
+               'rgba(153, 102, 255, 1)',
+               'rgba(255, 159, 64, 1)'
+           ],
+           borderWidth: 1
+       }]
+   },
+   options: {
+       maintainAspectRatio: false,
+       scales: {
+           yAxes: [{
+               ticks: {
+                   beginAtZero:true
+               }
+           }]
+       }
+   },
+   responsive: true
+});
+
+$mobileUsersCanvas.parent().css({'width': '80%','height': '70%'});
+
+// Sample 2
+
+//    var data = {
+//   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+//   datasets: [{
+//     label: "Dataset #1",
+//     backgroundColor: "rgba(255,99,132,0.2)",
+//     borderColor: "rgba(255,99,132,1)",
+//     borderWidth: 2,
+//     hoverBackgroundColor: "rgba(255,99,132,0.4)",
+//     hoverBorderColor: "rgba(255,99,132,1)",
+//     data: [65, 59, 20, 81, 56, 55, 40],
+//   }]
+// };
+//
+// var options = {
+//   maintainAspectRatio: false,
+//   scales: {
+//     yAxes: [{
+//       stacked: true,
+//       gridLines: {
+//         display: true,
+//         color: "rgba(255,99,132,0.2)"
+//       }
+//     }],
+//     xAxes: [{
+//       gridLines: {
+//         display: false
+//       }
+//     }]
+//   }
+// };
+//
+// Chart.Bar('chart', {
+//   options: options,
+//   data: data
+// });
