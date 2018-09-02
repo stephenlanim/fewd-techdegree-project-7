69a5ad002f012dbac699aedc5fc69af665edb479
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-09-02 00:19:31 -0400

    Refactored alert message scripts for fewer lines of code. Placed scripts in alerts.js.

---
+++ js/alerts.js
@@ -0,0 +1,37 @@
+/* ======================================
+    Alert Message Scripts
+
+      Note: Styles can be found in alert.css
+   ====================================== */
+   
+// Function for creating an alert message
+function alertMessage(alertType, placementBefore, message) {
+
+  // create div element for alert message
+  // alertType = document.createElement('DIV');
+  let $alertType = $('<div></div>');
+
+  // add class and ID to alert message for styling and DOM manipulation
+  $alertType.attr('class', `${alertType}`).attr('id', `${alertType}`);
+  // $alertType.append('class="' + alertType + '"');
+
+  // Insert message text into alert message
+  $alertType.html(message);
+
+  // Insert alert message before DOM object
+  $alertType.insertBefore('#' + `${placementBefore}`);
+
+  // Start alert message as hidden upon page load, then display it
+  $('#' + `${alertType}` + '')
+    .hide()
+    .slideDown(500);
+
+  // When alert message is clicked...
+  $alertType.on('click', function(){
+
+    // Remove alert message
+    $alertType.remove();
+
+  });
+
+}
--- js/app.js
+++ js/app.js
@@ -2,33 +2,13 @@
     Dashboard
    ====================================== */
 
-/* ===== Alert ===== */
+/* ===== Normal Alert ===== */
+// See alerts.js for source code
+const notificationMsg = '<span><strong>Alert:</strong> You have 1 unread message. Check notifications.</span> <span class="close-x">&times;</span>';
 
-// create div element for flash alert message
-let flashAlert = document.createElement('DIV');
-let $flashAlert = $(flashAlert);
+// Create an alert message box, place it before the widgets section, and insert the above alert message.
+alertMessage('alert', 'widgets', notificationMsg);
 
-// add class and ID to alert message for styling and DOM manipulation
-$flashAlert.attr('class', 'alert').attr('id', 'flashAlert');
-
-// Insert message text into alert message
-$flashAlert.html('<span><strong>Alert:</strong> You have 1 unread message. Check notifications.</span> <span class="close-x">&times;</span>');
-
-// Insert alert message after div.topper
-$flashAlert.insertAfter('.topper');
-
-// Start alert message as hidden upon page load, then display it
-$('#flashAlert')
-  .hide()
-  .slideDown(500);
-
-// When alert message is clicked...
-$flashAlert.on('click', function(){
-
-  // Remove alert message
-  $flashAlert.remove();
-
-});
 
 /* ===== Traffic Chart ===== */
 
@@ -189,64 +169,25 @@ $messageForm.on('submit', function (e) {
   if ($searchUser.val() === '' || $messageUser.val() === '') {
 
     /* ----- Error Alert ----- */
+    // See alerts.js for source code
+    const errorMessage = 'You must provide both a message recipient and a message before submitting the form.</span> <span class="close-x">&times;</span>';
 
-    // create div element for error message
-    let errorMsg = document.createElement('DIV');
-    let $errorMsg = $(errorMsg);
-
-    // add ID or class to confirmation div
-    $errorMsg.attr('class', 'error').attr('id', 'errorMsg');
-
-    // Insert error text into confirmation
-    $errorMsg.html('You must provide both a message recipient and a message before submitting the form.</span> <span class="close-x">&times;</span>');
-
-    // Insert error message before message form
-    $errorMsg.insertBefore($messageForm);
-
-    // Start error message as hidden upon page load, then display it
-    $('#errorMsg')
-      .hide()
-      .slideDown(500);
-
-    // When error message is clicked,
-    $errorMsg.on('click', function(){
-      // Remove error message
-      $errorMsg.remove();
-
-    });
+    // Create error message box, place it before the Message User form, and insert the above error message.
+    alertMessage('error', 'messageForm', errorMessage);
 
   }
   else {
-    const $errorMsg = $('#errorMsg');
+    const $errorMsg = $('#error');
 
     // Remove error message
     $errorMsg.remove();
 
     /* ----- Confirmation Alert ----- */
+    // See alerts.js for source code
+    const confirmationMessage = 'Your message was sent successfully.</span> <span class="close-x">&times;</span>';
 
-    // create div element for confirmation message
-    let msgSent = document.createElement('DIV');
-    let $msgSent = $(msgSent);
+    // Create confirmation message box, place it before the Message User form, and insert the above confirmation message.
+    alertMessage('confirmation', 'messageForm', confirmationMessage);
 
-    // add ID or class to confirmation div
-    $msgSent.attr('class', 'confirmation').attr('id', 'msgSent');
-
-    // Insert message text into confirmation
-    $msgSent.html('Your message was sent successfully.</span> <span class="close-x">&times;</span>');
-
-    // Insert confirmation message before message form
-    $msgSent.insertBefore($messageForm);
-
-    // Start confirmation message as hidden upon page load, then display it
-    $('#msgSent')
-      .hide()
-      .slideDown(500);
-
-    // When confirmation message is clicked,
-    $msgSent.on('click', function(){
-      // Remove confirmation message
-      $msgSent.remove();
-
-    });
   }
 });
