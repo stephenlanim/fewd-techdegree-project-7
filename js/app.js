38482b0dcbc2e5b81edf20852285e330581aaa73
Stephen Anim <stephen.lloyd.anim@gmail.com>
2018-09-05 00:00:47 -0400

    Created notification list and programmed it to appear below bell icon when bell is clicked.
    Also added toggle-switch-css-map to css folder to stop error message in console.
    Added box shadow mixin.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,27 @@
     Dashboard Functionality
    ====================================== */
 
+/* ===== Notification List ===== */
+const $notifList = $('#notifList');
+
+// When alert bell is clicked
+$('.bell').on('click', function() {
+  
+  // If notification list is not visible...
+  if ($notifList.css('display') === 'none') {
+    // Show notification list
+    $notifList.slideDown(300);
+  }
+  // else, if notification list is visible
+  else {
+    // Hide the notification list
+    $notifList.slideUp(300);
+  }
+});
+
 /* ===== User Alert ===== */
 // See alerts.js for source code.
-const notificationMsg1 = '<span><strong>Alert:</strong> You have 1 unread message. Check notifications.</span> <span class="close-x">&times;</span>';
+const notificationMsg1 = '<span><strong>Alert:</strong> You have <strong>3</strong> unread notifications.</span> <span class="close-x">&times;</span>';
 
 // Create an alert message box, place it before the widgets section, and insert the above alert message.
 alertMessage('alert', 'userAlerts', notificationMsg1);
